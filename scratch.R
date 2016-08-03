348ade98a196a45f63bab87f97f16db7be94539e
Chris Quigley <cquigley@outlook.com>
2016-08-03 13:34:39 -0700

    package updates

---
+++ scratch.R
@@ -0,0 +1,12 @@
+# install.packages("devtools")
+ library("devtools")
+# devtools::install_github("klutometis/roxygen")
+# library(roxygen2)
+
+#setwd("z:/")
+#create("rPerfFunc2")
+
+setwd("z:/rPerfFunc")
+document()
+
+install_github('rPerfFunc','cquigley')
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rPerfFunc)
+
+test_check("rPerfFunc")
